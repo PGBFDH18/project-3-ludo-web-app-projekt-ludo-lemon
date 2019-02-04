cdeea5d45a267c8a5f58adb32656c5ab375406c7
Per G <pgg.kinna@gmail.com>
2019-02-04 22:56:45 +0100

    Implementerat JS-fil för LudoBoard. Kan mha JS ta reda på vilken pjäs som klickats. Har även lagt uppgiften om currentPlayer som variabel, för att visas på sidan.

---
+++ src/Lemon-WebApp/Lemon-WebApp/wwwroot/js/LudoBoard.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+    
+    var bricks = document.getElementsByClassName("brick");
+    var goBtn = document.getElementById("rollDiceButton");
+
+    for (var i = 0; i < bricks.length; i++) {
+        bricks[i].addEventListener("click", function (event) {
+            var pieceClicked = event.target.innerHTML;
+
+            console.log(event.target);
+        });
+    }
+
+    goBtn.addEventListener("click", function () {
+        var player = "Player" + getPlayer().toString()
+        printPlayerTurn(player);
+    })
+
+    function getPlayer() {
+        return Math.floor(Math.random() * 4) + 1;
+    }
+
+    function printPlayerTurn(player) {
+        document.getElementById("currentPlayer").innerHTML = player;
+    }
+
+})();
