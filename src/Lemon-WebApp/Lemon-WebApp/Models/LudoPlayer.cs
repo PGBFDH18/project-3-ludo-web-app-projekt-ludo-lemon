761f20ecb4c70aa47a8c2c4d0be3bd159c6b6794
tomasvaro <tomas.varo63@gmail.com>
2019-02-15 11:47:36 +0100

    LAgt till Model Validations

--- src/Lemon-WebApp/Lemon-WebApp/Models/LudoPlayer.cs
+++ src/Lemon-WebApp/Lemon-WebApp/Models/LudoPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Lemon_WebApp.Models
     public class LudoPlayer
     {
         public int playerId { get; set; }
+        [Required]
+        [StringLength(5-15)]
         public string name { get; set; }
         public int playerColor { get; set; }
         public List<Piece> pieces { get; set; }
