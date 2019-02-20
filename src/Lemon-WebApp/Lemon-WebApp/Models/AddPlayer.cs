c879ecd91c2b29b94d9d88790c953222cd6c066e
cjda <carljohan.danling@gmail.com>
2019-02-20 23:27:32 +0100

    Sista försöket, sedan vågar jag inte röra mer.

---
+++ src/Lemon-WebApp/Lemon-WebApp/Models/AddPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lemon_WebApp.Models
+{
+    public class AddPlayer
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public int gameId { get; set; }
+    }
+}
