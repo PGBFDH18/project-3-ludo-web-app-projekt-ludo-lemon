20896d3cf2d314bc4c8abe3c46913e1ff880985d
tomasvaro <tomas.varo63@gmail.com>
2019-02-26 17:47:34 +0100

    Flyttat valideringen från LudoPlayers.cs till AddPlayers.cs

--- src/Lemon-WebApp/Lemon-WebApp/Models/LudoPlayer.cs
+++ src/Lemon-WebApp/Lemon-WebApp/Models/LudoPlayer.cs
@@ -9,9 +9,6 @@ namespace Lemon_WebApp.Models
     public class LudoPlayer
     {
         public int playerId { get; set; }
-        [Required(ErrorMessage = "Please enter a user name.")]
-        [RegularExpression(@"^[a-zA-ZåäöüßÅÄÖÜ]+$", ErrorMessage = "Name can only contain letters")]
-        [StringLength(5-15, ErrorMessage = "The First Name must be more than {1} characters and less tha {2}.")]
         public string name { get; set; }
         public int playerColor { get; set; }
         public int Number { get; set; }
